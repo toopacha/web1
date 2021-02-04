59344bbecdf6c877c6454d3ce6aed23354470225
toopacha <66077661+toopacha@users.noreply.github.com>
2021-02-04 10:45:12 +0900

    3

---
+++ colors.js
@@ -0,0 +1,35 @@
+var Links = {
+  setColor: function(color){
+  var alist = document.querySelectorAll('a');
+  var i = 0;
+  while(i < alist.length){
+     alist[i].style.color = 'color';
+     i = i+ 1;
+   }
+   }
+}
+
+var Body = {
+ setColor:function (color){
+   document.querySelector('body').style.color = color;
+},
+SetBackgroundColor:function (color){
+ document.querySelector('body').style.backgroundColor = color;
+}
+}
+function nightDayHandler(self){
+var target = document.querySelector('body');
+if (self.value === 'night'){
+Body.SetBackgroundColor('black');
+Body.setColor('white');
+self.value = 'day';
+
+Link.ssetColor('powderblue')
+} else {
+Body.SetBackgroundColor('white');
+Body.setColor('black');
+self.value ='night';
+
+Link.ssetColor('blue')
+ }
+}
